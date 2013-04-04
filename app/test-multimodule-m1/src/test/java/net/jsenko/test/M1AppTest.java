daf9160d52452ed94e598e6c886b130b6f8f9e9b
Jakub Senko <jsenko@redhat.com>
2013-04-04 12:23:06 +0200

    Initial commit.

---
+++ app/test-multimodule-m1/src/test/java/net/jsenko/test/M1AppTest.java
@@ -0,0 +1,31 @@
+package net.jsenko.test;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class M1AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public M1AppTest( String testName )
+    {
+        super( testName );
+    }
+
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
